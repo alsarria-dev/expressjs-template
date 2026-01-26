d95b8bacb12e11119f25cd5a295b8df21f2eab01
Alvaro <alvaro.sarria.rico@gmail.com>
2026-01-26 22:04:28 +0100

    Commenting

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,7 @@
+// Import user data model to work with mongoDB collection
 import User from "../models/User.model.js";
 
+// Retrieve n number of users information
 export const getUsers = (limit) => {
   return async (req, res) => {
     const users = await User.find().limit(limit);
