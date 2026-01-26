f65247f4481e9257544812e9a37e2bc9fca5c717
Alvaro <alvaro.sarria.rico@gmail.com>
2026-01-26 22:05:45 +0100

    Commenting

--- models/User.model.js
+++ models/User.model.js
@@ -1,5 +1,7 @@
+// Import mongoose functions to define and validate data models
 import { Schema, model } from "mongoose";
 
+// Define my user data Schema
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
